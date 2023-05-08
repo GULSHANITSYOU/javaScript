9463e34c5a4f595e53943f524f6fc0b5d2befe0c
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2023-05-08 06:00:20 +0530

    swicth statement in js

---
+++ BASICS/swicthStatmente.js
@@ -0,0 +1,15 @@
+let x = 0 ; 
+
+switch(x){
+    case 1: console.log(x); 
+    break ; 
+    case 3: console.log(x); 
+    break ; 
+    case 5: console.log(x) ; 
+    break;
+    case 7: console.log(x) ; 
+    break ;
+    case 9 : console.log(x) ; 
+    break ; 
+    default : console.log("try again") ; 
+}
