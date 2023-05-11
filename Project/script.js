c35b763cae82206506f1c48a3ab003de5f43bc69
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2023-05-11 16:05:16 +0530

    interface of Quize app is ready

---
+++ Project/script.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+        question: "Which is larget animal in the world?", 
+        answers:[
+            {text: "Shar" , correct: false},
+            {text: "Blue whale" , correct: true},
+            {text: "Elepphant" , correct: false},
+            {text: "Giraffe" , correct: false},
+        ]
+    },
+
+    {
+        question: "Which is the smallest country in the world?", 
+        answers:[
+            {text: "vatican city " , correct: true},
+            {text: "Bhutan" , correct: false},
+            {text: "Nepal" , correct: false},
+            {text: "Shri Lanka" , correct: false},
+        ]
+    },
+
+    {
+        question: "Which is larget desert in the world?", 
+        answers:[
+            {text: "Kalahari" , correct: false},
+            {text: "Gobi" , correct: false},
+            {text: "Sahara" , correct: false},
+            {text: "Antranctica" , correct: true},
+        ]
+    },
+
+    {
+        question: "Which is the smallest continent in the world?", 
+        answers:[
+            {text: "Asia" , correct: false},
+            {text: "Australia" , correct: true},
+            {text: "Arctic" , correct: false},
+            {text: "Africa" , correct: false},
+        ]
+    }
+]; 
